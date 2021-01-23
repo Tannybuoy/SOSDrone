fe7c739b00e029752ddf74ed5cde4437132dcaee
Tannybuoy <jagger1948@gmail.com>
2021-01-24 01:30:32 +0530

    sos intro

---
+++ objectdetect.py
@@ -0,0 +1,37 @@
+import os, io
+from google.cloud import vision
+from google.cloud.vision import types
+from Pillow_Utility import draw_borders, Image
+import pandas as pd
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']=r'ServiceAccountToken.json'
+client = vision.ImageAnnotatorClient()
+
+file_name = 'ikea3.jpg'
+image_path = os.path.join('.\Images', file_name)
+
+with io.open(image_path, 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.types.Image(content=content)
+response = client.object_localization(image=image)
+localized_object_annotations = response.localized_object_annotations
+
+pillow_image = Image.open(image_path)
+df = pd.DataFrame(columns=['name', 'score'])
+for obj in localized_object_annotations:
+    df = df.append(
+        dict(
+            name=obj.name,
+            score=obj.score
+        ),
+        ignore_index=True)
+    
+    r, g, b = random.randint(150, 255), random.randint(
+        150, 255), random.randint(150, 255)
+
+    draw_borders(pillow_image, obj.bounding_poly, (r, g, b),
+                 pillow_image.size, obj.name, obj.score)
+
+print(df)
+pillow_image.show()
